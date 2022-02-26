749b5716051ecf9c733eff4e06158014c1b19a51
David Charif <davidcharif@hotmail.com>
2022-02-26 09:49:40 -0500

    https link clould

--- src/helpers/fileUpload.js
+++ src/helpers/fileUpload.js
@@ -1,5 +1,5 @@
 export const fileUpload = async  (file) => {
- const cloudUrl ='http://api.Cloudinary.com/v1_1/davidcharif/image/upload'
+ const cloudUrl ='https://api.Cloudinary.com/v1_1/davidcharif/image/upload'
  const formData = new FormData()
  formData.append('upload_preset','cloudinaryUpload')
  formData.append('file',file)
